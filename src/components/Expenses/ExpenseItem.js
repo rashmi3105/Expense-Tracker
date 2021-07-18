a5fa4ad7aaa533e7ce1196b2ca48ce721a5e2ae3
rashmi3105 <rash.rashmikumari@gmail.com>
2021-07-18 21:45:01 +0530

    Version 1.0

---
+++ src/components/Expenses/ExpenseItem.js
@@ -0,0 +1,17 @@
+import ExpenseDate from "./ExpenseDate";
+import "./ExpenseItem.css";
+import Card from "../UI/Card";
+
+function ExpenseItem(props) {
+  return (
+    <Card className="expense-item">
+      <ExpenseDate date={props.date} />
+      <div className="expense-item__description">
+        <h2>{props.title}</h2>
+      </div>
+      <div className="expense-item__price">₹{props.amount}</div>
+    </Card>
+  );
+}
+
+export default ExpenseItem;
